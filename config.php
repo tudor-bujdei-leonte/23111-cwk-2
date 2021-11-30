c14831228487b787e1d698c928f49bc13c4f9c46
Tudor Bujdei-Leonte <tudor.bujdei-leonte@student.manchester.ac.uk>
2021-11-30 05:35:10 +0000

    Error message

--- config.php
+++ config.php
@@ -16,4 +16,6 @@ $link = mysqli_connect($database_host, $database_user, $database_pass, 'n98211tb
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+echo $link;
 ?>
