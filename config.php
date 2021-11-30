46c7d2ed1c21b924c52ec65fb92f5d4aa9a907c5
Tudor Bujdei-Leonte <tudor.bujdei-leonte@student.manchester.ac.uk>
2021-11-30 05:21:47 +0000

    Server data change

--- config.php
+++ config.php
@@ -1,12 +1,17 @@
 <?php
+
+require_once('config.inc.php');
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'n98211tb');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
+$link = mysqli_connect($database_host, $database_user, $database_pass, $group_dbnames[0]);
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
